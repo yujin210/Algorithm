0fcaf31ab577bd294f1f8d7214078c45d998ca54
yujin210 <geneee950@gmail.com>
2021-09-17 23:28:13 +0900

    update today algorithm

--- algorithm/src/array/Main.java
+++ algorithm/src/array/Main.java
@@ -21,7 +21,6 @@ import java.util.Scanner;
 public class Main {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		
 		int[] arr = new int[10];
 		for(int i=0; i<arr.length; i++) {
 			arr[i] = sc.nextInt();
