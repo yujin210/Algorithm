0ed236f3e7ccd718a53f6968c840e36fa09ec190
yujin210 <geneee950@gmail.com>
2021-10-09 17:35:17 +0900

    til algorithm

---
+++ algorithm/src/jungol/Jungol163.java
@@ -0,0 +1,17 @@
+package jungol;
+
+public class Jungol163 {
+	public static void main(String[] args) {
+		int[][] arr = {{3, 5, 9}, {2, 11, 5}, {8, 30, 10}, {22, 5, 1}};
+		int sum = 0;
+		
+		for(int i = 0; i < arr.length; i++) {
+			for(int j = 0; j < arr[i].length; j++) {
+				System.out.print(arr[i][j] + " ");
+				sum += arr[i][j];
+			}
+			System.out.println();
+		}
+		System.out.println(sum);
+	}
+}
