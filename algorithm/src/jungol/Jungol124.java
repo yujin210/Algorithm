efbc8e4ddfe59308990e7936c6525b063b6baeb2
yujin210 <geneee950@gmail.com>
2021-10-17 01:49:07 +0900

    til algorithm

---
+++ algorithm/src/jungol/Jungol124.java
@@ -0,0 +1,37 @@
+package jungol;
+
+import java.util.Scanner;
+
+public class Jungol124 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int month = sc.nextInt();
+		
+		switch(month) {
+			case 1 :
+			case 3 :
+			case 5 :
+			case 7 :
+			case 8 :
+			case 10 : 
+			case  12 : 
+				System.out.println(31);
+				break;
+				
+			case 2 : 
+				System.out.println(28);
+				break;
+				
+			case 4 :
+			case 6 :
+			case 9 :
+			case 11 :
+				System.out.println(30);
+				break;
+				
+			default :
+				System.out.println("잘못된 입력입니다.");
+		}
+	}
+}
