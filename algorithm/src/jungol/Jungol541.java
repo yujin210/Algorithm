5fcdf4cdb73711c24d04a70aabbb3f417be6b6b4
yujin210 <geneee950@gmail.com>
2021-10-23 20:50:24 +0900

    til algorithm

---
+++ algorithm/src/jungol/Jungol541.java
@@ -0,0 +1,15 @@
+package jungol;
+
+import java.util.Scanner;
+
+public class Jungol541 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		char ch = sc.next().charAt(0);
+		
+		for(int i = 1; i <= 20; i++) {
+			System.out.print(ch);
+		}
+	}
+}
