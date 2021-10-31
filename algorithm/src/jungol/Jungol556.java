08b141b6f25897097fd52e894f237a32965e7aa5
yujin210 <geneee950@gmail.com>
2021-11-01 01:52:00 +0900

    til algorithm

---
+++ algorithm/src/jungol/Jungol556.java
@@ -0,0 +1,12 @@
+package jungol;
+
+public class Jungol556 {
+	public static void main(String[] args) {
+		int[] arr = new int[10];
+		
+		for(int i = 0; i < 10; i++) {
+			arr[i] = (i+1);
+			System.out.printf("%d ", arr[i]);
+		}
+	}
+}
