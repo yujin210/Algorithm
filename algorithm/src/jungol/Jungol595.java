77476f0965b5e98e7ce5287219cfc15172ce7a83
yujin210 <geneee950@gmail.com>
2021-11-24 01:51:28 +0900

    til algorithm

---
+++ algorithm/src/jungol/Jungol595.java
@@ -0,0 +1,12 @@
+package jungol;
+
+public class Jungol595 {
+	public static void main(String[] args) {
+		String str = "Hong Gil Dong";
+		
+		System.out.print(str.charAt(3));
+		System.out.print(str.charAt(4));
+		System.out.print(str.charAt(5));
+		System.out.print(str.charAt(6));
+	}
+}
