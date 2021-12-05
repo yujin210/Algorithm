5985b432a5d25c03adfdabcd4bed877705636aeb
yujin210 <geneee950@gmail.com>
2021-12-05 17:13:49 +0900

    til algorithm

---
+++ algorithm/src/jungol/Jungol171.java
@@ -0,0 +1,19 @@
+package jungol;
+
+import java.util.Scanner;
+
+public class Jungol171 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int num = sc.nextInt();
+		sum(num);
+	}
+	static void sum(int num) {
+		int sum = 0;
+		for(int i = 1; i <= num; i++) {
+			sum += i;
+		}
+		System.out.println(sum);
+	}
+}
