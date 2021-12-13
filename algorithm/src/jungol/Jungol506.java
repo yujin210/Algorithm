35961264547014758ef0133d59572850ec5a3331
yujin210 <geneee950@gmail.com>
2021-12-13 22:20:41 +0900

    til algorithm

---
+++ algorithm/src/jungol/Jungol506.java
@@ -0,0 +1,13 @@
+package jungol;
+
+public class Jungol506 {
+	public static void main(String[] args) {
+		int height = 170;
+		double weight = 68.6;
+		
+		System.out.println("My height");
+		System.out.println(height);
+		System.out.println("My weight");
+		System.out.printf("%.06f", weight);
+	}
+}
