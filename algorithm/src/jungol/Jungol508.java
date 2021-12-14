957cce8e3b71f78c43239797654584af3c3d6e70
yujin210 <geneee950@gmail.com>
2021-12-14 22:42:11 +0900

    til algorithm

---
+++ algorithm/src/jungol/Jungol508.java
@@ -0,0 +1,10 @@
+package jungol;
+
+public class Jungol508 {
+	public static void main(String[] args) {
+		System.out.printf("%10s%10s%10s\n", "item", "count", "price");
+		System.out.printf("%10s%10d%10d\n", "pen", 20, 100);
+		System.out.printf("%10s%10d%10d\n", "note", 5, 95);
+		System.out.printf("%10s%10d%10d\n", "eraser", 110, 97);
+	}
+}
