69f30ef85ae36c0f4a4592772eaee79f682e6816
yujin210 <geneee950@gmail.com>
2021-12-24 23:45:46 +0900

    til algorithm

---
+++ algorithm/src/jungol/Jungol520.java
@@ -0,0 +1,15 @@
+package jungol;
+
+import java.util.Scanner;
+
+public class Jungol520 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int num = sc.nextInt();
+		
+		System.out.println(num++);
+		System.out.println(++num);
+	}
+
+}
---
+++ algorithm/src/jungol/Jungol523.java
@@ -0,0 +1,21 @@
+package jungol;
+
+import java.util.Scanner;
+
+public class Jungol523 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int num1 = sc.nextInt();
+		int num2 = sc.nextInt();
+		
+		System.out.print(num1 + " > " + num2 + " --- ");
+		System.out.println(num1>num2);
+		System.out.print(num1 + " < " + num2 + " --- ");
+		System.out.println(num1<num2);
+		System.out.print(num1 + " >= " + num2 + " --- ");
+		System.out.println(num1>=num2);
+		System.out.print(num1 + " <= " + num2 + " --- ");
+		System.out.println(num1<=num2);
+	}
+}
