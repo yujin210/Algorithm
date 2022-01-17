4875571bbc053392bdcda3654d5e60836321d84d
yujin210 <geneee950@gmail.com>
2022-01-17 23:51:38 +0900

    프로그래머스 - 짝수와 홀수

---
+++ algorithm/src/programmers/EvenAndOdd.java
@@ -0,0 +1,12 @@
+package programmers;
+
+class Solution3 {
+    public String solution(int num) {
+        String answer = "";
+        if (num%2==0)
+            answer = "Even";
+        else
+            answer = "Odd";
+        return answer;
+    }
+}
