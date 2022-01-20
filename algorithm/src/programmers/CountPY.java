b10698ede38eda3d7259f84275a03cc2c9071589
yujin210 <geneee950@gmail.com>
2022-01-21 00:13:47 +0900

    문자열 내 p와 y의 개수 - 풀이수정 1

--- algorithm/src/programmers/CountPY.java
+++ algorithm/src/programmers/CountPY.java
@@ -3,20 +3,21 @@ package programmers;
 class Solution {
     boolean solution(String s) {
         boolean answer = true;
-        int pCount = 0;
-        int yCount = 0;
-        char ch;
+        int count = 0; // p와 y의 개수를 각각 세었던 전과 달리, 변수를 하나만 사용
+        char ch; 
+        
+        s = s.toUpperCase(); // 문자열 s를 모두 대문자로 바꿔서 소문자와 대문자 구분이 필요없도록 함
         
         for(int i = 0; i < s.length(); i++) {
              ch = s.charAt(i);
-            if(ch == 'p' || ch == 'P') {
-                pCount++;
-            } else if(ch == 'y' || ch == 'Y') {
-                yCount++;
+            if(ch == 'P') {
+                count++; // p를 찾으면 count 증가
+            } else if(ch == 'Y') {
+                count--; // y를 찾으면 count 감소
             }
         }
         
-        answer = (pCount == yCount);
+        answer = (count == 0); // count가 0이면 p와 y의 개수가 동일하거나, 하나도 없는 경우이므로 true 반환 
         return answer;
     }
 }
