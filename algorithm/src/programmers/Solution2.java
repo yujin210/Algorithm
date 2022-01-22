4f63d1aa6cbd925d117a73a9bd0d82409514b897
yujin210 <geneee950@gmail.com>
2022-01-23 00:30:30 +0900

    없는 숫자 더하기 - 풀이수정 1

--- algorithm/src/programmers/Solution2.java
+++ algorithm/src/programmers/Solution2.java
@@ -3,13 +3,11 @@ package programmers;
 class Solution {
     public int solution(int[] numbers) {
         int answer = 45;
-        int sum = 0;
         
         for(int i = 0; i < numbers.length; i++) {
-            sum += numbers[i];
+            answer -= numbers[i];
         }
         
-        answer -= sum;
         return answer;
     }
 }
