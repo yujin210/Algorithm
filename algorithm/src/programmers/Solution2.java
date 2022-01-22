058cba8daf882ca6d8f74d14dad4b9c08f65f522
yujin210 <geneee950@gmail.com>
2022-01-23 00:27:30 +0900

    프로그래머스 - 없는 숫자 더하기

---
+++ algorithm/src/programmers/Solution2.java
@@ -0,0 +1,15 @@
+package programmers;
+
+class Solution {
+    public int solution(int[] numbers) {
+        int answer = 45;
+        int sum = 0;
+        
+        for(int i = 0; i < numbers.length; i++) {
+            sum += numbers[i];
+        }
+        
+        answer -= sum;
+        return answer;
+    }
+}
