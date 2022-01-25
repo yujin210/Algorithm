85b9a550e7e592a7d730b5f5ca91f5d39119945c
yujin210 <geneee950@gmail.com>
2022-01-25 23:06:23 +0900

    프로그래머스 - 예산

---
+++ algorithm/src/programmers/Budget.java
@@ -0,0 +1,23 @@
+package programmers;
+
+import java.util.Arrays;
+
+class Solution {
+    public int solution(int[] d, int budget) {
+        int answer = 0;
+        int total = 0;
+        Arrays.sort(d);
+        
+        for(int i = 0; i < d.length; i++) {
+            total += d[i];
+            answer++;
+            if(total > budget) {
+                answer--;
+                break;
+            } else if (total == budget) {
+                break;
+            }
+        }
+        return answer;
+    }
+}
