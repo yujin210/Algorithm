6d9f8a8cfa84f7fdad0e22324955d320702335f0
yujin210 <geneee950@gmail.com>
2022-01-30 01:22:56 +0900

    선형검색 - 책에서 설명하는 코드 (for 사용)

--- algorithm/src/doit/chap03/SeqSearch.java
+++ algorithm/src/doit/chap03/SeqSearch.java
@@ -5,21 +5,28 @@ import java.util.Scanner;
 public class SeqSearch {
 	// 요솟수가 n인 배열 a에서 key와 같은 요소를 선형 검색합니다.
 	static int seqSearch(int[] a, int n, int key) {
-		int i = 0;
-		
 		// 책에서 설명하는 배열 검색의 종료 조건은 두 가지이다.
 		// 1. 검색할 값을 발견하지 못하고 배열의 끝을 지나간 경우
 		// 2. 검색할 값과 같은 요소를 발견한 경우
 		
-		// 구현 방법 1 : while 사용
-		while(true) {
-			if(i == n)		// 1번에 해당 (i는 0부터 시작이고, n은 인덱스의 값이 아닌 배열 크기의 값이기 때문에 (i == n)이 성립하는 경우는 찾는 값이 배열에 존재하지 않아 n+1번 반복했을 때이다.) 
-				return -1;	// 검색 실패 (-1을 반환)
-			
-			if(a[i] == key)	// 2번에 해당 
-				return i;	// 검색 성공 (인덱스를 반환)
-			i++;
-		}
+		// 구현 방법 1 : while문 사용
+		/*
+		 * while(true) { 
+		 * 	if(i == n) // 1번에 해당 (i는 0부터 시작이고, n은 인덱스의 값이 아닌 배열 크기의 값이기 때문에 (i == n)이 성립하는 경우는 찾는 값이 배열에 존재하지 않아 n+1번 반복했을 때이다.) 
+		 		return -1; // 검색 실패 (-1을 반환)
+		 		
+		 * if(a[i] == key) // 2번에 해당 
+		 		return i; // 검색 성공 (인덱스를 반환) 
+		 	
+		 	i++; 
+		 	}
+		 */
+		
+		// 구현 방법 2 : for문 사용
+		for(int i = 0; i < n; i++)
+			if (a[i] == key)
+				return i;	// 검색 성공
+			return -1;		// 검색 실패		
 	}
 	
 	public static void main(String[] args) {
