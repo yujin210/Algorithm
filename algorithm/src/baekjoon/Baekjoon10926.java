b33bb1a46d0b0726bd4a5e5c35f8ef81e6f800be
yujin210 <geneee950@gmail.com>
2022-02-11 23:31:00 +0900

    백준 - ??!

---
+++ algorithm/src/baekjoon/Baekjoon10926.java
@@ -0,0 +1,12 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+public class Baekjoon10926 {
+	public static void main(String[] args) {
+	        Scanner sc = new Scanner(System.in);
+	        String id = sc.next();
+	        
+	        System.out.println(id + "??!");
+	    }
+}
