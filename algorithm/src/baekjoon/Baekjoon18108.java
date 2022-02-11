e598209b4ba768b0ece4d93fb90b7dc4dc437fdd
yujin210 <geneee950@gmail.com>
2022-02-12 01:25:21 +0900

    백준 - 1998년생인 내가 태국에서는 2541년생?!

---
+++ algorithm/src/baekjoon/Baekjoon18108.java
@@ -0,0 +1,13 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+public class Baekjoon18108 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int input = sc.nextInt();
+		
+		System.out.println(input - 543);
+	}
+}
