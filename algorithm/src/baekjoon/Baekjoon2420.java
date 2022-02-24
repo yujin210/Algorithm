2c02c92efafd592f5cc15e9d7855f92cbe0d50ce
yujin210 <geneee950@gmail.com>
2022-02-24 22:24:38 +0900

    백준 - 사파리월드

---
+++ algorithm/src/baekjoon/Baekjoon2420.java
@@ -0,0 +1,13 @@
+package baekjoon;
+
+import java.util.*;
+
+public class Baekjoon2420 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		long n = sc.nextLong();
+		long m = sc.nextLong();
+		
+		System.out.println(Math.abs(n-m));
+	}
+}
