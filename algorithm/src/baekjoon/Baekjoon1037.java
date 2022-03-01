de31410a395fac7bc9b8164fe6c7a1a6b2642785
yujin210 <geneee950@gmail.com>
2022-03-02 00:06:49 +0900

    백준 - 약수

---
+++ algorithm/src/baekjoon/Baekjoon1037.java
@@ -0,0 +1,21 @@
+package baekjoon;
+
+import java.util.Scanner;
+import java.util.Arrays;
+
+public class Baekjoon1037 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int n = sc.nextInt();
+		int[] arr = new int[n];
+		
+		for(int i = 0; i < n; i++) {
+			arr[i] = sc.nextInt();
+		}
+		
+		Arrays.sort(arr);
+		
+		System.out.println(arr[0] * arr[n-1]);
+	}
+}
