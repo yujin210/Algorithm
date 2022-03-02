a52ae481cac9f769c74685110091545b81aa91aa
yujin210 <geneee950@gmail.com>
2022-03-02 23:07:37 +0900

    백준 - 수 찾기

--- algorithm/src/baekjoon/Baekjoon1920.java
+++ algorithm/src/baekjoon/Baekjoon1920.java
@@ -26,7 +26,7 @@ public class Baekjoon1920 {
 		int idx = 0;
 		
 		for(int i = 0; i < m; i++) {
-			idx = Arrays.binarySearch(arrN, arrM[i]);
+			idx = binSearch(arrN, n, arrM[i]);
 			if(idx < 0)
 				answer[i] = 0;
 			else 
@@ -36,5 +36,23 @@ public class Baekjoon1920 {
 		for(int i : answer) {
 			System.out.println(i);
 		}
+	}
+	
+	static int binSearch(int[] arr, int size, int key) {
+		int pl = 0;
+		int pr = size - 1;
+		
+		do {
+			int pc = (pl + pr) / 2;
+			if(arr[pc] == key) { // 검색 성공 
+				return pc;
+			} else if (arr[pc] > key) { // 찾는 값보다 중앙 값이 더 클 때 
+				pr = pc-1; // 검색 범위를 앞쪽 절반으로 줄임 
+			} else { // 찾는 값이 중앙 값보다 더 클 때 
+				pl = pc+1; // 검색 범위를 뒤쪽 절반으로 줄임 
+			}
+		} while (pl <= pr);
+
+		return -1; // 검색 실패 
 	}
 }
