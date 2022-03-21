bd2faeaf4c5e33790f7c32a823e5c5f8a97a6355
yujin210 <geneee950@gmail.com>
2022-03-21 12:17:20 +0900

    백준 - Hello Judge

---
+++ algorithm/src/baekjoon/Baekjoon9316.java
@@ -0,0 +1,15 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+public class Baekjoon9316 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int n = sc.nextInt();
+		
+		for(int i = 1; i <= n; i++) {
+			System.out.println("Hello World, Judge " + i + "!");
+		}
+	}
+}
