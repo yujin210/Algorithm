3137e1563ff822172587c00c48c87093e91dff58
yujin210 <geneee950@gmail.com>
2022-05-04 22:52:53 +0900

    백준 15963 : CASIO

---
+++ algorithm/src/baekjoon/Baekjoon15963.java
@@ -0,0 +1,15 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+public class Baekjoon15963 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		long n = sc.nextLong();
+		long m = sc.nextLong();
+		
+		System.out.println(n == m ? 1 : 0);
+	}
+}
