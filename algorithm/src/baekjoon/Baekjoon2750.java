62c6c0f30f3a5f50e58ea9448bbe861bd995c437
yujin210 <geneee950@gmail.com>
2022-05-08 16:11:06 +0900

    백준 2750 : 수 정렬하기

---
+++ algorithm/src/baekjoon/Baekjoon2750.java
@@ -0,0 +1,23 @@
+package baekjoon;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Baekjoon2750 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int n = sc.nextInt();
+		int[] arr = new int[n];
+		
+		for(int i = 0; i < n; i++) {
+			arr[i] = sc.nextInt();
+		}
+		
+		Arrays.sort(arr);
+		
+		for(int i : arr) {
+			System.out.println(i);
+		}
+	}
+}
