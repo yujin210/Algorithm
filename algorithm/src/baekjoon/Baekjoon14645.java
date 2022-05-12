e4482874fc7a365c7d26c0c7c4c59c54da374dd3
yujin210 <geneee950@gmail.com>
2022-05-12 23:33:26 +0900

    백준 14645 : 와이버스 부릉부릉

---
+++ algorithm/src/baekjoon/Baekjoon14645.java
@@ -0,0 +1,19 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+public class Baekjoon14645 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int busStop = sc.nextInt();
+		int passenger = sc.nextInt();
+		
+		for(int i = 0; i < busStop; i++) {
+			int in = sc.nextInt();
+			int out = sc.nextInt();
+		}
+		
+		System.out.println("비와이");
+	}
+}
