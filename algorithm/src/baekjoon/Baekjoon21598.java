dc28c15fba9dbde9d1a8363fdb7c3a001ae72af7
yujin210 <geneee950@gmail.com>
2022-05-17 21:13:16 +0900

    백준 21598 : SciComLove

---
+++ algorithm/src/baekjoon/Baekjoon21598.java
@@ -0,0 +1,15 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+public class Baekjoon21598 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int n = sc.nextInt();
+		
+		for(int i = 0; i < n; i++) {
+			System.out.println("SciComLove");
+		}
+	}
+}
